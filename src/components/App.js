a122902c7da569e34aaa7313f53a5dd8e0522ae6
rmamchyk <roman.mamchyk@gmail.com>
2020-10-17 14:47:30 +0300

    implement custom software page

--- src/components/App.js
+++ src/components/App.js
@@ -7,44 +7,52 @@ import Header from './ui/Header';
 import Footer from './ui/Footer';
 import LandingPage from './LandingPage';
 import Services from './Services';
+import CustomSoftware from './CustomSoftware';
+
 
 export default () => {
-  const [value, setValue] = useState(0);
-  const [selectedIndex, setSelectedIndex] = useState(0);
+    const [value, setValue] = useState(0);
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header
-          value={value}
-          setValue={setValue}
-          selectedIndex={selectedIndex}
-          setSelectedIndex={setSelectedIndex}
-        />
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={ () => <LandingPage setValue={setValue} setSelectedIndex={setSelectedIndex} />}
-          ></Route>
-          <Route
-            exact
-            path="/services"
-            render={() => <Services setValue={setValue} setSelectedIndex={setSelectedIndex} />}
-          ></Route>
-          <Route exact path="/custom-software" component={() => 'Custom Software'}></Route>
-          <Route exact path="/mobile-apps" component={() => 'Mobile Apps'}></Route>
-          <Route exact path="/websites" component={() => 'Websites'}></Route>
-          <Route exact path="/revolution" component={() => 'Revolution'}></Route>
-          <Route exact path="/about" component={() => 'About Us'}></Route>
-          <Route exact path="/contact" component={() => 'Contact Us'}></Route>
-          <Route exact path="/estimate" component={() => 'Estimate'}></Route>
-        </Switch>
-        <Footer
-          setValue={setValue}
-          setSelectedIndex={setSelectedIndex}
-        />
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Header
+                    value={value}
+                    setValue={setValue}
+                    selectedIndex={selectedIndex}
+                    setSelectedIndex={setSelectedIndex}
+                />
+
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        render={() => <LandingPage setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
+                    <Route
+                        exact
+                        path="/services"
+                        render={() => <Services setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
+                    <Route
+                        exact
+                        path="/custom-software"
+                        render={() => <CustomSoftware setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
+                    <Route exact path="/mobile-apps" component={() => 'Mobile Apps'} />
+                    <Route exact path="/websites" component={() => 'Websites'} />
+                    <Route exact path="/revolution" component={() => 'Revolution'} />
+                    <Route exact path="/about" component={() => 'About Us'} />
+                    <Route exact path="/contact" component={() => 'Contact Us'} />
+                    <Route exact path="/estimate" component={() => 'Estimate'} />
+                </Switch>
+
+                <Footer
+                    setValue={setValue}
+                    setSelectedIndex={setSelectedIndex}
+                />
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 };
--- src/components/ui/theme.js
+++ src/components/ui/theme.js
@@ -5,69 +5,74 @@ const arcOrange = '#ffba60';
 const arcGrey = '#868686';
 
 export default createMuiTheme({
-  palette: {
-    common: {
-      blue: arcBlue,
-      orange: arcOrange
-    },
-    primary: {
-      main: arcBlue
-    },
-    secondary: {
-      main: arcOrange
-    }
-  },
-  typography: {
-    tab: {
-      fontFamily: 'Raleway',
-      textTransform: 'none',
-      fontWeight: 700,
-      fontSize: '1rem',
-    },
-    estimateButton: {
-      fontFamily: 'Pacifico',
-      fontSize: '1.125rem',
-      textTransform: 'none',
-      color: 'white',
-      paddingLeft: 20,
-      paddingRight: 20
-    },
-    h2: {
-      fontFamily: 'Raleway',
-      fontWeight: 700,
-      fontSize: '2.5rem',
-      color: arcBlue,
-      lineHeight: 1.5
-    },
-    h3: {
-      fontFamily: 'Pacifico',
-      fontSize: '2.5rem',
-      color: arcBlue
-    },
-    h4: {
-      fontFamily: 'Raleway',
-      fontSize: '1.75rem',
-      color: arcBlue,
-      fontWeight: 'bold'
-    },
-    subtitle1: {
-      color: arcGrey,
-      fontSize: '1.25rem',
-      fontWeight: 300
-    },
-    subtitle2: {
-      color: 'white',
-      fontSize: '1.25rem',
-      fontWeight: 300
-    },
-    learnButton: {
-      borderColor: arcBlue,
-      color: arcBlue,
-      borderWidth: 2,
-      textTransform: 'none',
-      borderRadius: 50,
-      fontFamily: 'Roboto',
-      fontWeight: 'bold'
-    }
-  }
+	palette: {
+		common: {
+			blue: arcBlue,
+			orange: arcOrange
+		},
+		primary: {
+			main: arcBlue
+		},
+		secondary: {
+			main: arcOrange
+		}
+  	},
+  	typography: {
+		tab: {
+			fontFamily: 'Raleway',
+			textTransform: 'none',
+			fontWeight: 700,
+			fontSize: '1rem',
+		},
+		estimateButton: {
+			fontFamily: 'Pacifico',
+			fontSize: '1.125rem',
+			textTransform: 'none',
+			color: 'white',
+			paddingLeft: 20,
+			paddingRight: 20
+		},
+		h2: {
+			fontFamily: 'Raleway',
+			fontWeight: 700,
+			fontSize: '2.5rem',
+			color: arcBlue,
+			lineHeight: 1.5
+		},
+		h3: {
+			fontFamily: 'Pacifico',
+			fontSize: '2.5rem',
+			color: arcBlue
+		},
+		h4: {
+			fontFamily: 'Raleway',
+			fontSize: '1.75rem',
+			color: arcBlue,
+			fontWeight: 'bold'
+		},
+		subtitle1: {
+			color: arcGrey,
+			fontSize: '1.25rem',
+			fontWeight: 300
+		},
+		subtitle2: {
+			color: 'white',
+			fontSize: '1.25rem',
+			fontWeight: 300
+		},
+		body1: {
+			fontSize: '1.25rem',
+			color: arcGrey,
+			fontWeight: 300
+		},
+		learnButton: {
+			borderColor: arcBlue,
+			color: arcBlue,
+			borderWidth: 2,
+			textTransform: 'none',
+			borderRadius: 50,
+			fontFamily: 'Roboto',
+			fontWeight: 'bold'
+		}
+  	}
 });
